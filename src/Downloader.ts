83011d0440c9b6735c3a48de4b633ef05bb70f92
Scripter36 <1350adwx@gmail.com>
2022-05-23 01:55:26 +0900

    Downloader과 Scheduler 제작, 버그 수정

---
+++ src/Downloader.ts
@@ -0,0 +1,107 @@
+import { LatLng } from '@googlemaps/google-maps-services-js';
+import NaverMap from './data/NaverMap.js';
+import * as fs from 'fs/promises';
+import * as path from 'path';
+import dateFormat from 'dateformat';
+import KakaoMap from './data/KakaoMap.js';
+import GoogleMap from './data/GoogleMap.js';
+import SeoulSubwayCrawler from './data/SeoulSubway.js';
+
+function sleep(t: number) {
+  return new Promise((resolve) => setTimeout(resolve, t));
+}
+
+export default class Downloader {
+  private _naverURL: string;
+  kakaoURL: string;
+  googleData: { origin: LatLng; destination: LatLng };
+  private _busList: string[];
+  private _busIdList: string[];
+  private busPromise: Promise<void> | null;
+  savePath: string;
+
+  constructor(
+    params: Partial<{
+      naverURL: string;
+      kakaoURL: string;
+      googleData: { origin: LatLng; destination: LatLng };
+      busList: string[];
+      savePath: string;
+    }>
+  ) {
+    this.kakaoURL = params.kakaoURL;
+    this.googleData = params.googleData;
+    this.naverURL = params.naverURL;
+    this.savePath = params.savePath;
+    this.busList = params.busList ?? [];
+  }
+
+  async loadMap() {
+    const results = {
+      naver: await NaverMap.pointToPointByURL(this.naverURL, new Date()),
+      kakao: await KakaoMap.getPubTransRouteByURL(this.kakaoURL),
+      google: await GoogleMap.directions(this.googleData.origin, this.googleData.destination),
+      time: Date.now()
+    };
+    await fs.writeFile(
+      path.join(this.savePath, `map_${dateFormat(new Date(), 'yyyymmdd_HHMMss')}.json`),
+      JSON.stringify(results)
+    );
+  }
+
+  async loadTransitData() {
+    if (this.busPromise != null) await this.busPromise;
+    const results = {
+      subway: {
+        data: await SeoulSubwayCrawler.loadTrainInfo(),
+        time: Date.now()
+      }
+    };
+    await Promise.all(
+      this._busIdList.map(async (v, i) => {
+        await sleep(i * 1000);
+        results[this._busList[i]] = {
+          data: await KakaoMap.getBusInfo(this._busIdList[i]),
+          time: Date.now()
+        };
+      })
+    );
+    await fs.writeFile(
+      path.join(this.savePath, `transit_${dateFormat(new Date(), 'yyyymmdd_HHMMss')}.json`),
+      JSON.stringify(results)
+    );
+  }
+
+  set naverURL(naverURL: string) {
+    this._naverURL = naverURL;
+    if (this.googleData == null && this._naverURL != null) {
+      [this.googleData.origin, this.googleData.destination] = NaverMap.parseDirectionsURL(this._naverURL).map(
+        (param) => ({
+          lat: param.coords[1],
+          lng: param.coords[0]
+        })
+      );
+    }
+  }
+
+  get naverURL() {
+    return this._naverURL;
+  }
+
+  set busList(busList: string[]) {
+    this._busList = busList;
+    this.busPromise = (async () => {
+      this._busIdList = [];
+      for (let i = 0; i < busList.length; i++) {
+        const response = await KakaoMap.search(busList[i]);
+        this._busIdList.push(response.bus.find((busResult) => busResult.BUS_NAME === busList[i]).DOCID);
+        if (i != busList.length - 1) await sleep(1000);
+      }
+      this.busPromise = null;
+    })();
+  }
+
+  get busList() {
+    return this._busList;
+  }
+}
---
+++ src/Scheduler.ts
@@ -0,0 +1,141 @@
+import dateFormat from 'dateformat';
+import Downloader from './Downloader';
+
+const getGCD = (a: number, b: number) => {
+  while (a % b !== 0) {
+    const c = a % b;
+    a = b;
+    b = c;
+  }
+  return b;
+};
+
+export default class Scheduler {
+  mapLoadInterval: number; // 예상 도착 시간 포함 지도 데이터 불러오는 간격. 밀리세컨드
+  transitDataLoadInterval: number; // 실시간 대중교통 정보 불러오는 간격. 밀리세컨드
+  downloaders: Downloader[];
+
+  timer: NodeJS.Timer | null;
+  UITimer: NodeJS.Timer | null;
+
+  progress = 0;
+  mapProgress = 0;
+  mapMax = 0;
+  transitProgress = 0;
+  transitMax = 0;
+  interval = 0;
+  enabledTimeRanges: { begin?: number; end?: number }[];
+
+  statistics = {
+    transit: 0,
+    lastTransit: new Date(),
+    map: 0,
+    lastMap: new Date()
+  };
+
+  constructor(
+    params: Partial<{
+      mapLoadInterval: number;
+      transitDataLoadInterval: number;
+      downloaders: Downloader[];
+      enabledTimeRanges: { begin?: number; end?: number }[];
+    }>
+  ) {
+    this.mapLoadInterval = params.mapLoadInterval;
+    this.transitDataLoadInterval = params.transitDataLoadInterval;
+    this.downloaders = params.downloaders ?? [];
+    this.enabledTimeRanges = params.enabledTimeRanges;
+  }
+
+  static makeTime(hours: number, minutes: number, seconds: number) {
+    return (hours * 3600 + minutes * 60 + seconds) * 1000;
+  }
+
+  init() {
+    this.interval = getGCD(this.mapLoadInterval, this.transitDataLoadInterval);
+    this.mapMax = this.mapLoadInterval / this.interval;
+    this.transitMax = this.transitDataLoadInterval / this.interval;
+    this.progress = 0;
+    this.mapProgress = 0;
+    this.transitProgress = 0;
+    this.statistics = {
+      transit: 0,
+      lastTransit: new Date(),
+      map: 0,
+      lastMap: new Date()
+    };
+  }
+
+  start() {
+    this.stop();
+    this.resume();
+  }
+
+  pause() {
+    if (this.timer != null) clearInterval(this.timer);
+    if (this.UITimer != null) clearInterval(this.UITimer);
+  }
+
+  resume() {
+    this.pause();
+    this.timer = setInterval(() => {
+      const now = new Date();
+      const nowTime = Scheduler.makeTime(now.getHours(), now.getMinutes(), now.getSeconds());
+      if (
+        this.enabledTimeRanges.length > 0 &&
+        !this.enabledTimeRanges.some((range) => {
+          return (range.begin == null || nowTime >= range.begin) && (range.end == null || nowTime <= range.end);
+        })
+      )
+        return;
+      this.transitProgress++;
+      this.mapProgress++;
+      if (this.transitProgress >= this.transitMax) {
+        this.downloaders[0].loadTransitData();
+        this.statistics.transit++;
+        this.statistics.lastTransit = new Date();
+        this.transitProgress = 0;
+      }
+      if (this.mapProgress >= this.mapMax) {
+        this.downloaders.forEach((downloader, i) =>
+          setTimeout(() => downloader.loadMap(), i * this.transitDataLoadInterval)
+        );
+        this.statistics.map++;
+        this.statistics.lastMap = new Date();
+        this.mapProgress = 0;
+      }
+    }, this.interval);
+    this.UITimer = setInterval(() => {
+      process.stdout.write(
+        '\x1Bc========== SeoulPublicTransportAnalyzer ==========\n' +
+          `대중교통 정보: ${this.statistics.transit}회 수집 ${
+            this.statistics.transit > 0
+              ? `(마지막 수집 시간: ${dateFormat(this.statistics.lastTransit, 'yyyy-mm-dd HH:MM:ss')})`
+              : ''
+          }\n` +
+          `지도 정보: ${this.statistics.map}회 수집 ${
+            this.statistics.map > 0
+              ? `(마지막 수집 시간: ${dateFormat(this.statistics.lastMap, 'yyyy-mm-dd HH:MM:ss')})`
+              : ''
+          }\n` +
+          `데이터 수집 시간: ${
+            this.enabledTimeRanges.length === 0
+              ? '항상'
+              : this.enabledTimeRanges
+                  .map(
+                    (range) =>
+                      `${range.begin == null ? '00:00:00' : dateFormat(new Date(range.begin), 'UTC:HH:MM:ss')}~${
+                        range.end == null ? '24:00:00' : dateFormat(new Date(range.end), 'UTC:HH:MM:ss')
+                      }`
+                  )
+                  .join(', ')
+          }`
+      );
+    }, 1000);
+  }
+
+  stop() {
+    this.pause();
+    this.init();
+  }
+}
