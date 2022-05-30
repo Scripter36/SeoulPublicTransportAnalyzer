fe7d24c5458297aa8efb5828c2ae4aab7dedb11c
Scripter36 <1350adwx@gmail.com>
2022-05-31 01:51:32 +0900

    GoogleMap에서 alternative 옵션 활성화 및 departure_time 추가

--- src/data/GoogleMap.ts
+++ src/data/GoogleMap.ts
@@ -4,15 +4,17 @@ import 'dotenv/config';
 const client = new Client({});
 
 export default class GoogleMap {
-  static async directions(origin: LatLng, destination: LatLng) {
+  static async directions(origin: LatLng, destination: LatLng, departure_time?: Date) {
     const response = await client.directions({
       params: {
         origin,
         destination,
         key: process.env.GOOGLE_MAPS_API_KEY,
-        mode: TravelMode.transit
+        mode: TravelMode.transit,
+        alternatives: true,
+        departure_time
       },
-      timeout: 1000
+      timeout: 2500
     });
     return response.data;
   }
